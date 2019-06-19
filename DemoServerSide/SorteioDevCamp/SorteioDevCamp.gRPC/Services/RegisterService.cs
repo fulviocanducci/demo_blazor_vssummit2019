39411820cc9f35ba6052e863a139b28037d57012
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2019-06-18 22:42:18 -0300

    Adding the server side demo, with the registration for a lottery of 2 tickets for DevCamp. Still pending of integration with gRPC and some way to check requirements.

---
+++ DemoServerSide/SorteioDevCamp/SorteioDevCamp.gRPC/Services/RegisterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace SorteioDevCamp.gRPC.Services
+{
+    public class RegisterService : Register.RegisterBase
+    {
+        public override Task<RegisterReply> Store(RegisterRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new RegisterReply
+            {
+                Message = "OK"
+            });
+        }
+    }
+}
